c018b8e8e6a63c1b5f0f46173da2dd3fba4613c1
masalaza-cn <83977863+masalaza-cn@users.noreply.github.com>
2021-09-21 16:50:40 -0400

    Include year and high school

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello You</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Year: 2021 High School: LHS</h2>
     </div>
   );
 }
